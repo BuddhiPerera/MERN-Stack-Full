b1fbc5720d7a890d094798b3b4b30ddd4cae0d77
Buddhi Perera <67018688+BuddhiPerera@users.noreply.github.com>
2022-08-19 15:30:23 +0530

    created the model

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
-// import express from 'express';
-
+// imports;
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 // middleware
 app.use(cors());
 app.use(express.json()); // to convert data that pass through the body into json
+
+// database connection
 
 // routes
 app.post('/api/register',(req,res)=>{
