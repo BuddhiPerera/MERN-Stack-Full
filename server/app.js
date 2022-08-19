10302eeb4fd14e478ee1c1ea6d08aea4a28530df
Buddhi Perera <67018688+BuddhiPerera@users.noreply.github.com>
2022-08-19 16:25:01 +0530

    db connection & registration

--- server/app.js
+++ server/app.js
@@ -3,21 +3,49 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const User = require('./models/user');
 
 // middleware
 app.use(cors());
 app.use(express.json()); // to convert data that pass through the body into json
 
 // database connection
+mongoose.connect('mongodb://localhost:27017/mern-stack-full')
+
 
 // routes
-app.post('/api/register',(req,res)=>{
+// router for register
+app.post('/api/register', async (req, res) => {
     console.log(req.body);
-    res.json({status: 'ok'});
+    try {
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+        res.json({status: 'ok'});
+    } catch (err) {
+        console.log(err);
+        res.json({status: 'error', error: 'Duplicated Email Address'});
+    }
+});
+
+// router for login
+app.post('/api/login', async (req, res) => {
+        const user = await User.findOne({
+            email: req.body.email,
+            password: req.body.password
+        });
+        // if user found
+        if(user){
+            return res.json({status: 'ok', user: true});
+        } else{
+            return res.json({status: 'error', user: false});
+        }
 });
 
 // listen port
-app.listen(4000, ()=>{
+app.listen(4000, () => {
     console.log('listening on port 4000');
 });
 
